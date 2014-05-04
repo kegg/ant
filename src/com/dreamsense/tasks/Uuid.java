789eabc4ce45ebd35bb85838281b9264f664d639
kegg <kyle@dreamsense.com>
2014-05-04 15:57:51 -0600

    Added null checking for property.
    A property name is required when using this task. A BuildException will
    be thrown if one isn't specified.

--- src/com/dreamsense/tasks/Uuid.java
+++ src/com/dreamsense/tasks/Uuid.java
@@ -2,6 +2,7 @@ package com.dreamsense.tasks;
 
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.Project;
+import org.apache.tools.ant.BuildException;
 
 import java.util.UUID;
 
@@ -23,6 +24,8 @@ public class Uuid extends Task {
   }
 
   public void execute() {
+    if (getProperty() == null) throw new BuildException("A property name needs to be specified for your uuid.");
+
     project.setProperty(getProperty(), UUID.randomUUID().toString());
   }
 }
